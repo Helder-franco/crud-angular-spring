8aee6ff1eb88a1da279b90445b6d190e138f2283
Helder Franco <helderfranco2011@gmail.com>
2023-08-23 17:29:43 -0300

    :sparkles: find by id

--- crud-spring/src/main/java/com/heldev/controller/CourseController.java
+++ crud-spring/src/main/java/com/heldev/controller/CourseController.java
@@ -5,6 +5,7 @@ import com.heldev.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,13 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
